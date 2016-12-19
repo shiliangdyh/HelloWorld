0d57314a7f57776451071acf1de352be8b2ffa74
mirstone <mirstone@126.com>
2016-12-19 16:42:38 +0800

    修改tv

--- app/src/main/java/com/hf/helloworld/MainActivity.java
+++ app/src/main/java/com/hf/helloworld/MainActivity.java
@@ -2,12 +2,25 @@ package com.hf.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = ((TextView) findViewById(R.id.tv1));
+        mTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this, mTextView.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
